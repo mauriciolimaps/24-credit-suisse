3a52504e6e31c58d4b137e7cc752d5c017791079
Mauricio Lima <git@mauriciolima.eti.br>
2024-12-05 20:10:17 -0300

    Updates tabs in 'percentag-volume-test.cs' file

--- sources/tests/percentage-volume-test/percentage-volume-test.cs
+++ sources/tests/percentage-volume-test/percentage-volume-test.cs
@@ -6,8 +6,8 @@ using System.Collections.Generic;
 
 List<Operacao> operacoes = new()
 {
-	new Operacao { totalNegociado =       900.00m, porcentagem = 0.10M },
-	new Operacao { totalNegociado =     1_500.00m, porcentagem = 0.30M },
+    new Operacao { totalNegociado =       900.00m, porcentagem = 0.10M },
+    new Operacao { totalNegociado =     1_500.00m, porcentagem = 0.30M },
     new Operacao { totalNegociado =    23_456.78m, porcentagem = 0.30M },
     new Operacao { totalNegociado = 1_200_000.00m, porcentagem = 0.15M }
 };
@@ -18,9 +18,9 @@ Console.WriteLine("  +-------------------+-------------+----------------+");
 
 foreach (Operacao operacao in operacoes)
 {
-	Console.Write("  |  {0,15:N2}", operacao.totalNegociado);
+    Console.Write("  |  {0,15:N2}", operacao.totalNegociado);
     Console.Write("  |");
-	Console.Write("  {0,9:P1}",     operacao.porcentagem);
+    Console.Write("  {0,9:P1}",     operacao.porcentagem);
     Console.Write("  |"); 
     Console.Write("  {0,12}",       operacao.quantidade().ToString("N3"));
     Console.Write("  |");
@@ -31,28 +31,28 @@ Console.WriteLine("  +-------------------+-------------+----------------+");
 
 public class Operacao
 {
-	private decimal mPorcentagem;
-	public  decimal totalNegociado { get; set; }
-	public decimal porcentagem {
-		get { return mPorcentagem; }  
-		set
-		{ 
-			if ((value < 0) || (value >= 1))
-				throw new InvalidDataException("Porcentagem Inválida");
+    private decimal mPorcentagem;
+    public  decimal totalNegociado { get; set; }
+    public decimal porcentagem {
+        get { return mPorcentagem; }  
+        set
+        { 
+            if ((value < 0) || (value >= 1))
+                throw new InvalidDataException("Porcentagem Inválida");
 
-			mPorcentagem = value; 
-		}
-	}
+            mPorcentagem = value; 
+        }
+    }
 
-	public Operacao() { this.mPorcentagem = 0; }
+    public Operacao() { this.mPorcentagem = 0; }
 
-	public decimal quantidade()
-	{
-		if ((mPorcentagem < 0) || (mPorcentagem >= 1))
-			throw new Exception("O parâmetro de porcentagem deve ser positivo e menor que 1");
+    public decimal quantidade()
+    {
+        if ((mPorcentagem < 0) || (mPorcentagem >= 1))
+            throw new Exception("O parâmetro de porcentagem deve ser positivo e menor que 1");
 
-		decimal quantity = totalNegociado * porcentagem / (1 - porcentagem);
-		return quantity;
-	}
+        decimal quantity = totalNegociado * porcentagem / (1 - porcentagem);
+        return quantity;
+    }
 };
 
